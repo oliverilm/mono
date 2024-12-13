cf5e2b5f50d09a30e0e6b5362354bca3994175e4
oliver-sisutech <149678507+oliver-sisutech@users.noreply.github.com>
2024-12-13 15:24:18 +0200

    refactor

--- backend/core/src/app/services/club.ts
+++ backend/core/src/app/services/club.ts
@@ -10,81 +10,76 @@ export type SlugOrId = {
     id: string
 } | { slug: string }
 
-async function create({ name, country, userId }: ClubCreate & UserIdObject): Promise<Club | null> {
-    // PS: this action should require a subscription later on
-    // if user is subscribed then allow them to create a club
 
-    // the price should be dependant on the avg hosting bill in zone or something like that 
-    
-    const user = await prisma.userProfile.findUnique({ 
-        where: {
-            userId
-        }, 
-    })
-
-    if (user?.clubId !== null) {
-        throw new Error("User already belongs to a club")
-    }
-
-    
-    const slug = slugifyString(name)
-
-    if (!slug) {
-        throw new Error("name must contain letters and or numbers")
-    }
-
-    const club = await prisma.club.create({
-        data: {
-            name,
-            country,
-            slug,
-            description: "",
-            clubMetadata: { create: {}}
+export const ClubService = {
+    getClubByIdOrSlug: function (slugOrId: SlugOrId ): Promise<Club | null> | null {
+        if ("id" in slugOrId) {
+            return prisma.club.findUnique({
+                where: {
+                    id: slugOrId.id
+                }
+            })
         }
-    })
-    if (club) {
-        await prisma.userProfile.update({ 
+        if ("slug" in slugOrId) {
+            return prisma.club.findFirst({
+                where: {
+                    slug: slugOrId.slug
+                }
+            })
+        }
+        return null
+    },
+    getClubList: function ({ take = 25, skip = 0 }: SkipTake): Promise<Club[]> {
+        return prisma.club.findMany({
+            take: Number(take),
+            skip: Number(skip),
+            orderBy: {
+                id: "desc"
+            }
+        })
+    },
+    create: async function ({ name, country, userId }: ClubCreate & UserIdObject): Promise<Club | null> {
+        // PS: this action should require a subscription later on
+        // if user is subscribed then allow them to create a club
+    
+        // the price should be dependant on the avg hosting bill in zone or something like that 
+        
+        const user = await prisma.userProfile.findUnique({ 
             where: {
                 userId
             }, 
-            data: { 
-                clubId: club.id 
-            }
         })
-    }
-    return club || null
-}
-
-function getClubByIdOrSlug(slugOrId: SlugOrId ): Promise<Club | null> | null {
-    if ("id" in slugOrId) {
-        return prisma.club.findUnique({
-            where: {
-                id: slugOrId.id
-            }
-        })
-    }
-    if ("slug" in slugOrId) {
-        return prisma.club.findFirst({
-            where: {
-                slug: slugOrId.slug
-            }
-        })
-    }
-    return null
-}
-
-function getClubList({ take = 25, skip = 0 }: SkipTake): Promise<Club[]> {
-    return prisma.club.findMany({
-        take: Number(take),
-        skip: Number(skip),
-        orderBy: {
-            id: "desc"
+    
+        if (user?.clubId !== null) {
+            throw new Error("User already belongs to a club")
         }
-    })
-}
-
-export const ClubService = {
-    getClubByIdOrSlug,
-    getClubList,
-    create,
+    
+        
+        const slug = slugifyString(name)
+    
+        if (!slug) {
+            throw new Error("name must contain letters and or numbers")
+        }
+    
+        const club = await prisma.club.create({
+            data: {
+                name,
+                country,
+                slug,
+                description: "",
+                clubMetadata: { create: {}}
+            }
+        })
+        if (club) {
+            await prisma.userProfile.update({ 
+                where: {
+                    userId
+                }, 
+                data: { 
+                    clubId: club.id 
+                }
+            })
+        }
+        return club || null
+    },
 }
--- backend/core/src/app/services/competition.ts
+++ backend/core/src/app/services/competition.ts
@@ -6,85 +6,77 @@ import { Search, SkipTake } from "../schemas/common"
 import { CreateCompetition } from "../schemas/competition"
 import { prisma } from "../utils/db"
 
-
-
 export interface UpdateCompetition {
     name?: string
     isPublished?: boolean,
     isArchived?: boolean
 }
 
-async function createCompetition({data, userProfile}: { data: CreateCompetition, userProfile: UserProfile}) {
-    const slug = slugifyString(data.name)
-    
-    if (!userProfile.clubId) {
-        throw new Error("User does not belong to a club")
-    }
-
-    const club = await prisma.club.findUnique({
-        where: {
-            id: userProfile.clubId
-        },
-        select: {
-            name: true
-        }
-    })
-
-    if (!club) {
-        throw new Error("Invalid club")
-    }
-
-    const competition = await prisma.competition.create({
-        data: {
-            slug,
-            clubName: club?.name,
-            ...data,
-        }
-    })
-
-    if (!competition) {
-        throw new Error("Something went wrong")
-    }
-
-    return competition
-}
-
-async function updateCompetition(competitionSlug: string, data: UpdateCompetition ) {
-    const updatableData: UpdateCompetition & { slug?: string } = {}
-    Object.entries(data).forEach(([key, value]) => {
-        if (key === "name") {
-            updatableData["slug"] = slugifyString(value)
-        }
-
-        updatableData[key as keyof UpdateCompetition] = value
-    })
-
-    try {
-        return prisma.competition.update({
-            where: {
-                slug: competitionSlug
-            },
-            data: updatableData
-        })
-    } catch (error) {
-        tryHandleKnownErrors(error as Error)
-    }
-    
-}
-
-async function list({search, ...skipTake}: SkipTake & Search) {
-    return prisma.competition.findMany({
-       ...(search ? { where: {
-            name: {
-                contains: search
-            }
-        }} : {}),
-        ...convertSkipTake(skipTake)
-    })
-}
-
 export const CompetitionService = {
-    createCompetition,
-    list,
-    updateCompetition,
+    createCompetition: async function ({data, userProfile}: { data: CreateCompetition, userProfile: UserProfile}) {
+        const slug = slugifyString(data.name)
+        
+        if (!userProfile.clubId) {
+            throw new Error("User does not belong to a club")
+        }
+    
+        const club = await prisma.club.findUnique({
+            where: {
+                id: userProfile.clubId
+            },
+            select: {
+                name: true
+            }
+        })
+    
+        if (!club) {
+            throw new Error("Invalid club")
+        }
+    
+        const competition = await prisma.competition.create({
+            data: {
+                slug,
+                clubName: club?.name,
+                ...data,
+            }
+        })
+    
+        if (!competition) {
+            throw new Error("Something went wrong")
+        }
+    
+        return competition
+    },
+    list: async function ({search, ...skipTake}: SkipTake & Search) {
+        return prisma.competition.findMany({
+           ...(search ? { where: {
+                name: {
+                    contains: search
+                }
+            }} : {}),
+            ...convertSkipTake(skipTake)
+        })
+    },
+    updateCompetition: async function (competitionSlug: string, data: UpdateCompetition ) {
+        const updatableData: UpdateCompetition & { slug?: string } = {}
+        Object.entries(data).forEach(([key, value]) => {
+            if (key === "name") {
+                updatableData["slug"] = slugifyString(value)
+            }
+    
+            updatableData[key as keyof UpdateCompetition] = value
+        })
+    
+        try {
+            return prisma.competition.update({
+                where: {
+                    slug: competitionSlug
+                },
+                data: updatableData
+            })
+        } catch (error) {
+            tryHandleKnownErrors(error as Error)
+        }
+        
+    },
 }
